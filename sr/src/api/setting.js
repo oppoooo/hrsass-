f81dcc881ddde15dd936147408fbbb23b2768cf5
258canc <1071739052@qq.com>
2022-08-23 18:08:09 +0800

    修改

--- sr/src/api/setting.js
+++
@@ -1,9 +0,0 @@
-import request from '@/utils/request'
-// 获取公司信息
-export function getCompanyInfo(companyId) {
-  return request({
-    url: `/company/${companyId}`
-  })
-}
-
-
--- sr/src/filters/index.js
+++
@@ -1,5 +0,0 @@
-import dayjs from 'dayjs'
-
-export const formatTime = (val) => {
-  return dayjs(val).format('YYYY-MM-DD')
-}
